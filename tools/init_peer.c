ae25f11a9cf47fa6e15a67cc0dd3655468781ca7
Maryam5553 <maryammunim@ymail.com>
2025-07-17 17:14:36 +0200

    init one peer (generate key/certificates)

---
+++ tools/init_peer.c
@@ -0,0 +1,39 @@
+#include "init_peer.h"
+
+int init_peer(peer_info *peer_info)
+{
+    if (!file_exists(peer_info->pkey_filename))
+    {
+        printf("Generating %s...\n", peer_info->pkey_filename);
+        if (gen_keyfile(peer_info->pkey_filename) == 1)
+        {
+            fprintf(stderr, "Failed to generate %s.", peer_info->pkey_filename);
+            return 1;
+        }
+        printf("%s generated.\n", peer_info->pkey_filename);
+    }
+
+    if (!file_exists(peer_info->TLS_cert_filename))
+    {
+        printf("Requesting TLS certificate...\n");
+
+        EVP_PKEY *priv_key = read_key(peer_info->pkey_filename);
+        if (priv_key == NULL)
+        {
+            fprintf(stderr, "Couldn't read private key from %s.", peer_info->pkey_filename);
+            return 1;
+        }
+
+        if (request_TLS_certificate(priv_key, peer_info->CN, peer_info->root_cert_filename, peer_info->TLS_cert_filename) == 1)
+        {
+            fprintf(stderr, "Failed to request a TLS certificate.");
+            EVP_PKEY_free(priv_key);
+            return 1;
+        }
+
+        printf("TLS certificate and root certificate written in %s and %s.\n", peer_info->TLS_cert_filename, peer_info->root_cert_filename);
+        EVP_PKEY_free(priv_key);
+    }
+
+    printf("Done initializing peer.\n");
+}
---
+++ tools/init_peer.h
@@ -0,0 +1,19 @@
+#ifndef INIT_PEER_H
+#define INIT_PEER_H
+
+#include "gen_credentials.h"
+#include "request_TLS_certificate.h"
+
+typedef struct
+{
+    char *pkey_filename;      // peer private key
+    char *TLS_cert_filename;  // peer personate TLS certificate
+    char *root_cert_filename; // certificate sent by CA used as root cert for peer private cert
+    unsigned char *CN;        // Common Name to put in certificate subject
+} peer_info;
+
+// Checks if peer private key and TLS certificate exists. If private key doesn't exist, generates one.
+// If TLS certificate doesn't exist, requests a certificate from CA.
+int init_peer(peer_info *peer_info);
+
+#endif
